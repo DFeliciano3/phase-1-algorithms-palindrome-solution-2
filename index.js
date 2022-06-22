0e7806696b2bb6e8f94abe7c94dc551c94a094dd
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-06-22 13:15:29 -0400

    commit tree c9c2a02dacc49b7388f4799dfa828a2a869c0b39

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // iterate from the beginning to the middle 
+  for (let i = 0; i < word.length / 2; i++) {
+  // check for each letter to the corresponding letter from the end
+    const j = word.length - 1 - i;
+  // if any letters do not match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
+ }
 
 /* 
   Add your pseudocode here
+  if the first letter is the same as the last letter, and the second letter is the same
+  as the second last letter, etc. until we reach the middle 
+  return true
+
+  iterate from the beginning to the middle 
+    check for each letter to the corresponding letter from the end
+      if any letters do not match, return false
+
+  return true
 */
 
 /*
